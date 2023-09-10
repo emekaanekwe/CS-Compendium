22078532c27cffa7195adff115ca1d7bd1836038
emekaenshinyan <emeka.enshinyan@gmail.com>
2023-09-10 13:48:36 -0700

    add: loops

--- Python-ML/Python_GLossary/how_x_works/iterator.py
+++
@@ -1,7 +0,0 @@
-'''
-An object representing a stream of data. 
-Iterators are required to have an __iter__() 
-method that returns the iterator object itself 
-so every iterator is also iterable and may be 
-used in most places where other iterables are accepted.
-'''
---
+++ Python-ML/Python_Glossary/how_x_works/sequence_types/loops.py
@@ -0,0 +1,51 @@
+x = [1,2,3,4,5]
+y = ["one","two","three","four","five"]
+z = [[5], [5, 10], [10,20], [20,40], [40,80]]
+
+re = [1,1,2,2,3,3]
+
+
+x.remove(4)
+print(x)
+for i in x:
+    print(i)
+
+c = 0
+dict = {}
+    
+def count_common_elements(list1, list2):
+    # Create two dictionaries to store the element counts for each list
+    count_dict1 = {}
+    count_dict2 = {}
+
+    # Initialize counts for list1
+    for element in list1:
+        if element in count_dict1:
+            count_dict1[element] += 1
+        else:
+            count_dict1[element] = 1
+
+    # Initialize counts for list2
+    for element in list2:
+        if element in count_dict2:
+            count_dict2[element] += 1
+        else:
+            count_dict2[element] = 1
+
+    # Initialize a variable to keep track of the common elements count
+    common_count = 0
+
+    # Iterate through the keys in count_dict1 and check if they also exist in count_dict2
+    for key in count_dict1:
+        if key in count_dict2:
+            # Increment common_count with the minimum count of the element in both lists
+            common_count += min(count_dict1[key], count_dict2[key])
+
+    return common_count
+
+# Example usage
+list1 = [1, 2, 2, 3, 4, 5]
+list2 = [2, 2, 3, 4, 4, 5, 5, 6]
+
+common_elements_count = count_common_elements(list1, list2)
+print("Number of common elements:", common_elements_count)
