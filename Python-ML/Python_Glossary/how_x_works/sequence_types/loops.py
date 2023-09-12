8bd6bd598226f1d1e31715a617bad228e6d7706a
emekaenshinyan <emeka.enshinyan@gmail.com>
2023-09-11 20:59:18 -0700

    add: areas of focus

--- Python-ML/Python_Glossary/how_x_works/sequence_types/loops.py
+++ Python-ML/Python_Glossary/how_x_works/sequence_types/loops.py
@@ -2,7 +2,8 @@ x = [1,2,3,4,5]
 y = ["one","two","three","four","five"]
 z = [[5], [5, 10], [10,20], [20,40], [40,80]]
 
-re = [1,1,2,2,3,3]
+
+
 
 
 x.remove(4)
@@ -10,9 +11,22 @@ print(x)
 for i in x:
     print(i)
 
-c = 0
-dict = {}
+re = [1,1,2,3,3,4,5,5]
+def test(list1):
+    dict = {}
+    for i in list1:
+        #check if the element in list1 already exists in the dictionary
+        if i in dict:
+            dict[i] += 1
+        else:
+            dict[i] = 1
+
+        print(dict)
+
+test(re)
     
+#chat gpt answer
+'''
 def count_common_elements(list1, list2):
     # Create two dictionaries to store the element counts for each list
     count_dict1 = {}
@@ -49,3 +63,4 @@ list2 = [2, 2, 3, 4, 4, 5, 5, 6]
 
 common_elements_count = count_common_elements(list1, list2)
 print("Number of common elements:", common_elements_count)
+'''
