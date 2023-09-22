520ce0f6c5f98be63aed7f25e4a772f5332205e9
emekaenshinyan <emeka.enshinyan@gmail.com>
2023-09-22 05:09:44 -0700

    edit: misc.

--- Python-ML/Python_Glossary/how_x_works/classes/makingclasses.py
+++ Python-ML/Python_Glossary/how_x_works/classes/makingclasses.py
@@ -7,14 +7,14 @@ for CREATING objects (unlike Java where classes are objects)
     instances, and those objects have a set f attributes (data),
     and a set of methods (functions)
     
-Attributes - vars tat store data. they define the characteristics
+1) Attributes - vars tat store data. they define the characteristics
     or properties of objects created from class. They're like the 
     nouns and adjectives of language
 
-Methods - the actions/operations that define what the object can
+2) Methods - the actions/operations that define what the object can
     perform like the verbs
 
-Four Pillars of OOP
+3) Four Pillars of OOP
     Encapsulation - classes cover over both data and methods to hide
         the internal details of the class from outside, which allows
         information hiding and abstraction
@@ -32,9 +32,14 @@ Four Pillars of OOP
         which in turn creates an obj with its own attributes and
         methods based on the class constraints.
 
-Naming Conventions
+4) Naming Conventions
     Public Members - normal naming pattern
     Non-Public Members - _radius, _calculate_area()
+        these are not meant to be a part of the class's API
+        this only advises that the attribute is intended for
+        use only inside the class
+    Name Mangling - __<name>  are names that do not have 
+        direct access, thus not part of class's API
 
 
 '''
@@ -64,6 +69,10 @@ print(radOfC)
 #call the method that calculates the area
 areaOfC = circle.calcArea()
 print("area of circle:", areaOfC)
+
+#4
+publicname = 5
+_nonpublicname = 10
 
 
 
