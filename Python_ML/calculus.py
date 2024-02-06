8c6a6e8eb0effbf9f1a40bc47433771280adfffd
emekaenshinyan <emeka.enshinyan@gmail.com>
2024-02-07 08:23:10 +1100

    add: unicode list

--- Python_ML/calculus.py
+++ Python_ML/calculus.py
@@ -1,25 +1,38 @@
-class Calculus:
+'''
+The function will represent a rule in calc
+write a function that takes in args (a1...an), non-exhaustive
+'''
 
-   def __init__(self):
-     self.power_result = "dy/dx = "
-   
-   def power_rule(self, n, power):
-     print(f"the derivative of y = {n}x^{power} is")
+class Calculus:
+    def __init__(self):
+        #derivative result
+        self.power_result = "dy/dx = "
+    
+    
+    #POWER RULE
+    def power_rule(self, n, power):
+        print(f"the derivative of y = {n}x^{power} is")
     #if y = f(x) then y' = f'(x)
      #example inputs	
      #y = x^2 -> y' = 2x
      #y = (2x^3)
      #"y = (3x^2 + 2x + 1)
     
-     if type(power) != int and type(n) != int:
-      raise TypeError("the input for n and power must be integers.")
-     if n < 1:
-       raise ValueError("the formula input must be a positive integer.")
-     n = power
-     power = power - 1
-     formula = self.power_result+str(n)+"x^"+str(power)+""
-     return formula
+        if type(power) != int and type(n) != int:
+         raise TypeError("the input for n and power must be integers.")
+        if n < 1:
+          raise ValueError("the formula input must be a positive integer.")
+        n = power
+        power = power - 1
+        formula = self.power_result+str(n)+"x^"+str(power)+""
+        return formula
 
+    def integral(self):
+        print(f"the integral is the accumulation of quantities, i.e. the computation of the area under a curve")
+        print(f"There are two types of integrals: definite and indefinite")
+        
+
+#TESTS
 test_power_rule = Calculus()
 print("here's an example of the power rule:")
 user_n = int(input("enter a number for x: "))
---
+++ Python_ML/unicode_characters.py
@@ -0,0 +1,17 @@
+class Calculus_Unicode:
+    
+    def __init__(self, code):
+        self.code = ""
+    
+    def display_unicode(self):
+        unicode_list = []
+        #put codes in list
+            #ask input
+                #
+        print(self)
+
+#Print Unicodes
+unicode = Calculus_Unicode
+print(unicode.display_unicode("alpha -  \u03b1"))
+print(unicode.display_unicode("integral - \u222B; double - \u222c; triple - \u222d"))
+print(unicode.display_unicode("differential - \u2202"))
