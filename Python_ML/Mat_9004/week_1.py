5822e40d88f183a39901f3b458074180525528b3
emeka.enshinyan <emeka.enshinyan@gmail.com>
2024-02-29 18:01:29 +1100

    add: the algorithms repo

--- Python_ML/Mat_9004/week_1.py
+++ Python_ML/Mat_9004/week_1.py
@@ -14,6 +14,7 @@ There is nothing to be learnt from a Professor, which is not to be met with in B
 
 '''
 Glossary
+    Sets & intervals
     injective
     bijective
     inverse function
@@ -22,6 +23,8 @@ Glossary
     real-valued funct
     convex
     concave
+    linear funct
+    finding linear funct that best fits points
 
 Resources
     https://en.wikipedia.org/wiki/Convex_analysis
